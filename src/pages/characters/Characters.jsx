902ba4c0db4eae50d83ebd2e9633c727c2e6e10c
Лёха домашний <macarena7777777@gmail.com>
2024-10-25 23:37:57 +0300

    added sort

--- src/pages/characters/Characters.jsx
+++ src/pages/characters/Characters.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import MySelect from '../../components/Select/MySelect'
+import MyInput from '../../components/Input/MyInput'
+import styles from "./styles.module.css"
 
 const Characters = () => {
     return (
         <div>
-
+            <div className={styles.sort}>
+                <MyInput />
+                <MySelect />
+                <MySelect />
+                <MySelect />
+            </div>
         </div>
     )
 }
